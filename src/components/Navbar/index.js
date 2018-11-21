436f39ff8714a1a32932eead45cd5b2ef2d47f6a
Kinark <igormarcossi@gmail.com>
2018-11-21 16:58:45 -0200

    Added scrolled effect to the navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,13 +1,39 @@
 import React from 'react';
-import styles from './styles.scss';
+import PropTypes from 'prop-types';
+
+import { withContext } from '~/instances/context';
 
 import NavLinks from '~/components/NavLinks';
 
-const Navbar = () => (
-   <nav className={`${styles.navbar} center`}>
-      <ul>
-         <NavLinks />
-      </ul>
-   </nav>
-)
-export default Navbar;
+import styles from './styles.scss';
+
+class Navbar extends React.PureComponent {
+   static propTypes = {
+      context: PropTypes.shape({}).isRequired,
+   }
+
+   state = {
+      scrolled: false
+   }
+
+   componentDidMount = () => { this.scrolledHandler(); window.addEventListener('scroll', this.scrolledHandler) }
+
+   componentWillUnmount = () => window.removeEventListener('scroll', this.scrolledHandler)
+
+   scrolledHandler = () => this.setState({ scrolled: window.scrollY >= 400 })
+
+   render() {
+      const { context } = this.props
+      const { scrolled } = this.state
+      const scrolledClass = (context.inverted ? styles.bwScrolled : styles.scrolled)
+      return (
+         <nav className={`${styles.navbar} center ${scrolled ? scrolledClass : ''}`}>
+            <ul>
+               <NavLinks />
+            </ul>
+         </nav>
+      );
+   }
+}
+
+export default withContext(Navbar);
