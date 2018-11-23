f37371d321f1c73823eaf5f5025a09fecd412054
Kinark <igormarcossi@gmail.com>
2018-11-22 23:31:58 -0200

    Added blog generalVariable

--- src/components/NavLinks/index.js
+++ src/components/NavLinks/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 
+import { AppContext } from '~/instances/context';
+
 import NavItem from './components/NavItem';
 
 export default () => (
-   <React.Fragment>
-      <NavItem to="/">Home</NavItem>
-      <NavItem to="/games">Games</NavItem>
-      <NavItem to="/contact">Contact</NavItem>
-      <li>
-         <a target="_blank" rel="noopener noreferrer" href="https://medium.com/the-last-flame">BLOG</a>
-      </li>
-      <NavItem to="/about">About</NavItem>
-      {/* <NavItem to="/deeplake">Deeplake</NavItem> */}
-   </React.Fragment>
+   <AppContext.Consumer>
+      {context => (
+         <React.Fragment>
+            <NavItem to="/">Home</NavItem>
+            <NavItem to="/games">Games</NavItem>
+            <NavItem to="/contact">Contact</NavItem>
+            <li><a target="_blank" rel="noopener noreferrer" href={context.generalVariables.blog}>BLOG</a></li>
+            <NavItem to="/about">About</NavItem>
+            {/* <NavItem to="/deeplake">Deeplake</NavItem> */}
+         </React.Fragment>
+      )}
+   </AppContext.Consumer>
 )
